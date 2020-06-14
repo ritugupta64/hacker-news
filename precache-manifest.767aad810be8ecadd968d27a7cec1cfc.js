9cae48a79a44ab348accf342c14c39b18b8a73d2
ritugupta64 <31064674+ritugupta64@users.noreply.github.com>
2020-06-14 17:18:00 +0530

    Updates

---
+++ precache-manifest.767aad810be8ecadd968d27a7cec1cfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4acb5cd9a483fcd127ea3b5bc8d4b9d1",
+    "url": "/hacker-news/index.html"
+  },
+  {
+    "revision": "f777960847094e937aa0",
+    "url": "/hacker-news/static/css/main.0540c5a8.chunk.css"
+  },
+  {
+    "revision": "35f7e97abd9b491df4ab",
+    "url": "/hacker-news/static/js/2.0858a601.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hacker-news/static/js/2.0858a601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f777960847094e937aa0",
+    "url": "/hacker-news/static/js/main.0ef6070a.chunk.js"
+  },
+  {
+    "revision": "3b758c96ae057069d71d",
+    "url": "/hacker-news/static/js/runtime-main.c6904f2a.js"
+  }
+]);
